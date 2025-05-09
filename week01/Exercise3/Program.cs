742a53ffe5542e5713ab40b3382eef7a39315907
sirdave01 <d08178084956@gmail.com>
2025-05-09 10:26:09 -0700

    assignment part 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+    // this line of code requests for a user's guess and the magic number
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.Write("What is the magic number?: ");
+        string magicNumber = Console.ReadLine();
+
+        Console.Write("What is your guess?: ");
+        string guess = Console.ReadLine();
+
+        int magic = int.Parse(magicNumber);
+        int gues = int.Parse(guess);
+
+        if (gues == magic)
+        {
+            Console.WriteLine("You guessed the magic number!");
+        }
+        else if (gues < magic)
+        {
+            Console.WriteLine("Your guess is too low.");
+        }
+        else
+        {
+            Console.WriteLine("Your guess is too high.");
+        }
     }
 }
