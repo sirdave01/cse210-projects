09c82a6e052fe8f7a1198a2ece0c0fe04c0aa739
sirdave01 <d08178084956@gmail.com>
2025-05-09 11:40:56 -0700

    week1 exercise 5 done

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
+        // these lines of code are used to declare the variables as global variables to be used anywhere in the program
+
+        DisplayWelcome(); // Call the method to display the message
+
+        string UserName = PromptUserName(); // Call the method to get the user's name
+
+        int UserNumber = PromptUserNumber(); // Call the method to get the user's number
+
+        int SquaresNumber = SquareNumber(UserNumber); // Call the method to square the number
+
+        DisplayResult(UserNumber + SquaresNumber); // Call the method to display the result
+
+
+        // this line of code is used to create a method that displays a message
+
+    }
+
+    static void DisplayWelcome()
+    {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    }
+    // this line of code is used to create a method that prompts the user for their name 
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string fName = Console.ReadLine();
+        return fName;
+    }
+    // this line of code is used to create a method that prompts the user for their number
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your number? ");
+        string number = Console.ReadLine();
+        int userNumber = int.Parse(number);
+        return userNumber;
+    }
+    // this line of code is used to create a method that squares the user's number
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    // this line of code is used to create a method that displays the result
+    static void DisplayResult(int result)
+    {
+        Console.WriteLine($"The result is: {result}");
     }
 }
