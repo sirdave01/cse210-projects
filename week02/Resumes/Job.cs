26723a8fbefa714cb4a77d962026a27e59a135e8
sirdave01 <d08178084956@gmail.com>
2025-05-12 09:00:49 -0700

    did it?

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -11,21 +11,22 @@ public class Job
     public int _startYear;
     public int _endYear;
 
-    public Job(string jobTitle, string company, int startYear, int endYear)
-    {
-        _jobTitle = jobTitle;
-        _company = company;
-        _startYear = startYear;
-        _endYear = endYear;
-    }
+    // public Job(string jobTitle, string company, int startYear, int endYear)
+    // {
+    //     _jobTitle = jobTitle;
+    //     _company = company;
+    //     _startYear = startYear;
+    //     _endYear = endYear;
+    // }
 
     // this line of code is used to create a constructor that takes in the parameters jobTitle, company, startYear, and endYear
     // public Job(string jobTitle, string company, int startYear, int endYear)
-    Job job = new Job("Software Engineer", "Tech Corp", 2015, 2020);
     
+    // Job job = new Job("Software Engineer", "Tech Corp", 2015, 2020);
+
     // this line of code is used to create a method that displays the job information
     public void DisplayJob()
-    
+
     {
         Console.WriteLine($"{_jobTitle} at ({_company}) {_startYear} - {_endYear}");
     }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Tech Corp";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Console.WriteLine($"{job1._jobTitle} at ({job1._company}) {job1._startYear} - {job1._endYear}");
+
+        Job job2 = new Job();
+        job2._jobTitle = "Project Manager";
+        job2._company = "Business Inc";
+        job2._startYear = 2018;
+        job2._endYear = 2023;
+
+        Console.WriteLine($"{job2._jobTitle} at ({job2._company}) {job2._startYear} - {job2._endYear}");
+
+        // Console.WriteLine("Hello World! This is the Resumes Project.");
     }
 }
