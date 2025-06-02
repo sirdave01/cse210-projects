ff8e0153572433009fb3887adf26d33274fd6f27
sirdave01 <d08178084956@gmail.com>
2025-06-02 16:03:14 -0700

    sub class - WritingAssignment done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -11,7 +11,12 @@ class Program
         Assignment assignment2 = new Assignment("Roberto Rodriguez", "Fractions");
         MathAssignment mathAssignment2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
 
+        Assignment assignment3 = new WritingAssignment("Mary Jane", "European History", "The Causes of World War II");
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Jane", "European History", "The Causes of World War II");
+
         Console.WriteLine(assignment2.GetSummary());
         Console.WriteLine(mathAssignment2.GetHomeworkList());
+        Console.WriteLine(writingAssignment1.GetWritingInfo());
+        Console.WriteLine(assignment3.GetSummary());
     }
 }
--- week05/Homework/WritingAssignment.cs
+++ week05/Homework/WritingAssignment.cs
@@ -4,3 +4,30 @@ using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+
+public class WritingAssignment : Assignment
+{
+    private string _title = " ";
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    // Getter and setter for the private variable _title
+
+    public string GetTitle()
+    {
+        return _title;
+    }
+
+    public void SetTitle(string title)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInfo()
+    {
+        return $"{GetStudentName()} - {GetTopic()} - {_title}";
+    }
+}
